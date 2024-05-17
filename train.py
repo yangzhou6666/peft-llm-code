1d1bd909ff1a2c325d56413f3504fbfb06c6dfb7
yangzhou6666 <zyang@smu.edu.sg>
2024-05-17 10:38:43 +0800

    initialize the function to train models on sstubs dataset

--- train.py
+++ train.py
@@ -110,6 +110,123 @@ def load_model_and_tokenizer(args):
     return model, tokenizer
 
 
+def run_train_hotfix(args):
+    # load datasets
+    # TODO: now just one file for one bug type, need to combine multiple bug types when running
+    dataset = load_sstubs_train_dataset()
+    intent_column = "sub_condition" # prompt to show intent
+    code_column = "fix" # the python commands that serve as the target
+
+    # load models
+    model, tokenizer = load_model_and_tokenizer(args)
+
+
+    exit()
+    # Load dataset
+    if args.dataset == "joint":
+        dataset = load_conala_train_dataset()
+        codealpaca = load_codealpaca_train_dataset()
+        dataset["train"] = concatenate_datasets([dataset["train"], codealpaca["train"]])
+        dataset["validation"] = concatenate_datasets([dataset["validation"], codealpaca["validation"]])
+        dataset.shuffle(args.seed)
+    else:
+        dataset_loading_func = globals().get(f"load_{args.dataset}_train_dataset")
+        dataset = dataset_loading_func()
+
+    intent_column = "nl" # prompt to show intent
+    code_column = "cmd" # the python commands that serve as the target
+    print(dataset["train"], dataset["validation"])
+
+    for example in dataset["train"].select(range(3)):
+        print(f"### Instruction:\n{example[intent_column]}\n### Response:\n{example[code_column]}")
+        print("=" * 100)
+
+    # load model and tokenizer
+    model, tokenizer = load_model_and_tokenizer(args)
+
+    def preprocess_function(example):
+        """
+        # we tokenize, pad and truncate the samples in the following way:
+        #   <pad><pad>...### Instruction:\n<intent>\n### Answer:\n<snippet><eos>
+        #
+        #   - prompt tokens `<pad><pad>...<intent + \n>` are ignored in the computation of the loss (-100 labels)
+        #   - `<eos>` delimits the snippet and allows the model to have more focused predictions at inference
+        """
+        tokenized_target = tokenizer(example[code_column],
+                                     max_length=args.max_target_length - 1,
+                                     truncation=True,
+                                     # incoder adds eos token before the start of a sequence -> ignore
+                                     add_special_tokens=False)
+        tokenized_target["input_ids"] = tokenized_target["input_ids"] + [tokenizer.eos_token_id]
+        tokenized_target["attention_mask"] = tokenized_target["attention_mask"] + [1]
+
+        prompt = "### Instruction:\n" + example[intent_column] + "\n### Response:\n"
+        max_prompt_len = (args.max_input_length + args.max_target_length) - \
+                         len(tokenized_target["input_ids"])
+        model_inputs = tokenizer(prompt, max_length=max_prompt_len,  padding="max_length", truncation=True)
+
+        model_inputs["labels"] = [-100] * len(model_inputs["input_ids"]) + tokenized_target["input_ids"]
+        model_inputs["input_ids"] = model_inputs["input_ids"] + tokenized_target["input_ids"]
+        model_inputs["attention_mask"] = model_inputs["attention_mask"] + tokenized_target["attention_mask"]
+
+        return model_inputs
+
+    def preprocess_function_seq2seq(example):
+        prompt = "Generate Python code: ### Instruction:\n" + example[intent_column] + "\n### Response:\n"
+        model_inputs = tokenizer(prompt, max_length=args.max_input_length, padding="max_length", truncation=True)
+        labels = tokenizer(example[code_column], max_length=args.max_target_length, padding="max_length", truncation=True)
+
+        labels["input_ids"] = [l if l != tokenizer.pad_token_id else -100 for l in labels["input_ids"]]
+        model_inputs["labels"] = labels["input_ids"]
+
+        return model_inputs
+
+    tokenize_fn = preprocess_function_seq2seq if "codet5" in args.model_name_or_path else preprocess_function
+    
+    dataset = dataset.map(tokenize_fn,
+                          num_proc=args.num_workers,
+                          remove_columns=dataset["train"].column_names,
+                          desc="Generating samples features.")
+
+    n_samples = len(dataset["train"])
+    n_samples_per_step = args.batch_size * args.num_gpus * args.gradient_accumulation_steps
+    eval_steps = math.ceil((n_samples // n_samples_per_step) * args.ratio_samples_per_eval_step)
+
+    training_args_cls = Seq2SeqTrainingArguments if "codet5" in args.model_name_or_path else TrainingArguments
+    training_args = training_args_cls(
+        output_dir=args.run_dir,
+        evaluation_strategy="no",
+        save_strategy="no",
+        num_train_epochs=args.num_epochs,
+        per_device_train_batch_size=args.batch_size,
+        per_device_eval_batch_size=args.batch_size,
+        gradient_accumulation_steps=args.gradient_accumulation_steps,
+        warmup_ratio=0.05,
+        learning_rate=args.learning_rate,
+        weight_decay=args.weight_decay,
+        lr_scheduler_type=args.lr_scheduler_type,
+        optim="adafactor",
+        logging_strategy="steps",
+        logging_steps=10,
+        fp16=True,
+        report_to=["wandb"] if args.use_wandb else ["none"]
+    )
+    trainer_cls = Seq2SeqTrainer if "codet5" in args.model_name_or_path else Trainer
+    trainer = trainer_cls(
+        model=model,
+        args=training_args,
+        train_dataset=dataset["train"],
+        eval_dataset=dataset["validation"],
+        tokenizer=tokenizer,
+        data_collator=default_data_collator,
+    )
+    trainer.add_callback(SaveBestModelCallback(trainer, eval_steps))
+    eval_results = trainer.evaluate()
+    logger.info(f"Evaluation loss before training: {round(eval_results['eval_loss'], 4)}")
+    trainer.train()
+
+
+
 def run_train(args):
     if args.dataset == "joint":
         dataset = load_conala_train_dataset()
@@ -120,14 +237,16 @@ def run_train(args):
     else:
         dataset_loading_func = globals().get(f"load_{args.dataset}_train_dataset")
         dataset = dataset_loading_func()
-    code_column = "cmd"
-    intent_column = "nl"
+
+    intent_column = "nl" # prompt to show intent
+    code_column = "cmd" # the python commands that serve as the target
     print(dataset["train"], dataset["validation"])
 
     for example in dataset["train"].select(range(3)):
         print(f"### Instruction:\n{example[intent_column]}\n### Response:\n{example[code_column]}")
         print("=" * 100)
 
+    # load model and tokenizer
     model, tokenizer = load_model_and_tokenizer(args)
 
     def preprocess_function(example):
