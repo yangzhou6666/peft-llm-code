6b591dfc98743c1bff300feb6bff49611842a42f
yangzhou6666 <zyang@smu.edu.sg>
2024-05-25 21:38:34 +0800

    for loop to automate the pipeline

--- scripts/train_sstubs_hotfix.sh
+++ scripts/train_sstubs_hotfix.sh
@@ -1,110 +1,61 @@
-CUDA_VISIBLE_DEVICES=1 python main.py \
-  --model_name_or_path Salesforce/codegen-350M-multi \
-  --dataset sstubs \
-  --tuning_method ia3 \
-  --num_epochs 5 \
-  --batch_size 16 \
-  --gradient_accumulation_steps 2 \
-  --learning_rate 3e-4 \
-  --lora_r 8 \
-  --lora_alpha 16 \
-  --do_train
+#!/bin/bash
 
-CUDA_VISIBLE_DEVICES=1 python main.py \
-  --model_name_or_path Salesforce/codegen-350M-mono \
-  --dataset sstubs \
-  --tuning_method ia3 \
-  --num_epochs 5 \
-  --batch_size 16 \
-  --gradient_accumulation_steps 2 \
-  --learning_rate 3e-4 \
-  --lora_r 8 \
-  --lora_alpha 16 \
-  --do_train
+tuning_methods=("lora" "ia3" "qlora-8bit" "prefix-tuning" "qlora-4bit")
+loss_modes=("learn_added_penalize_deleted" "learn_added_unlearn_deleted" "learn_added" "all_after" "unlearn_deleted")
+model_types=("multi" "mono" "nl")
 
-CUDA_VISIBLE_DEVICES=1 python main.py \
-  --model_name_or_path Salesforce/codegen-350M-nl \
-  --dataset sstubs \
-  --tuning_method ia3 \
-  --num_epochs 5 \
-  --batch_size 16 \
-  --gradient_accumulation_steps 2 \
-  --learning_rate 3e-4 \
-  --lora_r 8 \
-  --lora_alpha 16 \
-  --do_train
-
-
-CUDA_VISIBLE_DEVICES=1 python main.py \
-  --model_name_or_path Salesforce/codegen-2B-multi \
-  --dataset sstubs \
-  --tuning_method ia3 \
-  --num_epochs 5 \
-  --batch_size 4 \
-  --gradient_accumulation_steps 2 \
-  --learning_rate 6e-5 \
-  --lora_r 8 \
-  --lora_alpha 16 \
-  --do_train
-
-CUDA_VISIBLE_DEVICES=1 python main.py \
-  --model_name_or_path Salesforce/codegen-2B-mono \
-  --dataset sstubs \
-  --tuning_method ia3 \
-  --num_epochs 5 \
-  --batch_size 4 \
-  --gradient_accumulation_steps 2 \
-  --learning_rate 6e-5 \
-  --lora_r 8 \
-  --lora_alpha 16 \
-  --do_train
-
-CUDA_VISIBLE_DEVICES=1 python main.py \
-  --model_name_or_path Salesforce/codegen-2B-nl \
-  --dataset sstubs \
-  --tuning_method ia3 \
-  --num_epochs 5 \
-  --batch_size 4 \
-  --gradient_accumulation_steps 2 \
-  --learning_rate 6e-5 \
-  --lora_r 8 \
-  --lora_alpha 16 \
-  --do_train
+# 使用for循环遍历所有调优方法
+for method in "${tuning_methods[@]}"
+do
+  for loss_mode in "${loss_modes[@]}"
+  do
+    echo "Running training with tuning method: $method and loss mode: $loss_mode"
+    CUDA_VISIBLE_DEVICES=1 python main.py \
+      --model_name_or_path Salesforce/codegen-2B-multi \
+      --dataset sstubs \
+      --tuning_method $method \
+      --num_epochs 5 \
+      --batch_size 4 \
+      --gradient_accumulation_steps 2 \
+      --learning_rate 3e-4 \
+      --lora_r 8 \
+      --lora_alpha 16 \
+      --do_train \
+      --loss_mode $loss_mode
+  done
+done
 
 
 
-# CUDA_VISIBLE_DEVICES=1 python main.py \
-#   --model_name_or_path Salesforce/codegen-6B-multi \
-#   --dataset sstubs \
-#   --tuning_method ia3 \
-#   --num_epochs 5 \
-#   --batch_size 1 \
-#   --gradient_accumulation_steps 2 \
-#   --learning_rate 3e-5 \
-#   --lora_r 8 \
-#   --lora_alpha 16 \
-#   --do_train
 
-# CUDA_VISIBLE_DEVICES=1 python main.py \
-#   --model_name_or_path Salesforce/codegen-6B-mono \
-#   --dataset sstubs \
-#   --tuning_method ia3 \
-#   --num_epochs 5 \
-#   --batch_size 1 \
-#   --gradient_accumulation_steps 2 \
-#   --learning_rate 3e-5 \
-#   --lora_r 8 \
-#   --lora_alpha 16 \
-#   --do_train
+for method in "${tuning_methods[@]}"
+do
+  for loss_mode in "${loss_modes[@]}"
+  do
+    echo "Running training with tuning method: $method and loss mode: $loss_mode"
+    CUDA_VISIBLE_DEVICES=1 python main.py \
+      --model_name_or_path Salesforce/codegen-2B-mono \
+      --dataset sstubs \
+      --tuning_method $method \
+      --num_epochs 5 \
+      --batch_size 4 \
+      --gradient_accumulation_steps 2 \
+      --learning_rate 3e-4 \
+      --lora_r 8 \
+      --lora_alpha 16 \
+      --do_train \
+      --loss_mode $loss_mode
+  done
+done 
 
-# CUDA_VISIBLE_DEVICES=1 python main.py \
-#   --model_name_or_path Salesforce/codegen-6B-nl \
-#   --dataset sstubs \
-#   --tuning_method ia3 \
-#   --num_epochs 5 \
-#   --batch_size 1 \
-#   --gradient_accumulation_steps 2 \
-#   --learning_rate 3e-5 \
-#   --lora_r 8 \
-#   --lora_alpha 16 \
-#   --do_train
+# for method in "${tuning_methods[@]}"
+# do
+#   for loss_mode in "${loss_modes[@]}"
+#   do
+#     echo "Running training with tuning method: $method and loss mode: $loss_mode"
+#   CUDA_VISIBLE_DEVICES=0 python sstubs_eval.py \
+#       --model_name_or_path Salesforce/codegen-350M-multi  \
+#       --adapter_path runs/checkpoints/$loss_mode/sstubs/codegen-350M-multi_$method \
+#       --tuning_method $method
+#   done
+# done
